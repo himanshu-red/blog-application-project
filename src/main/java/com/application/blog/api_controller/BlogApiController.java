0c167ee7d933ca8538563b3d59328b608bb72b80
himanshu_red <himanshutdw98@gmail.com>
2023-01-13 10:39:53 +0530

    added creating timestamp and update timestamp in blog entity class

--- src/main/java/com/application/blog/api_controller/BlogApiController.java
+++ src/main/java/com/application/blog/api_controller/BlogApiController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,13 @@ public class BlogApiController {
     @GetMapping("/blogs")
     public List<Blog> getBlogs(){
         return blogService.getAllBlogs(); 
+    }
+
+    @PostMapping("/blogs")
+    public String saveBlog(@RequestBody Blog blog){
+        System.out.println("");
+        blog.setId(0);
+        long blogId = blogService.saveBlog(blog); 
+        return "blog saved with id " + blogId; 
     }
 }
--- src/main/java/com/application/blog/entity/Blog.java
+++ src/main/java/com/application/blog/entity/Blog.java
@@ -1,10 +1,16 @@
 package com.application.blog.entity;
 
+import java.security.Timestamp;
+import java.time.Instant;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,7 +29,9 @@ public class Blog {
     private long id; 
     private String author; 
     private String exerpt; 
-    private String created_at; 
-    private String updated_at; 
+    @CreationTimestamp
+    private Timestamp createdAt; 
+    @UpdateTimestamp
+    private Timestamp updatedAt; 
     private String content; 
 }
