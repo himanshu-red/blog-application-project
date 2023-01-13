fd612eb21f76abb4e0d9c93a8d504fa9055924f9
himanshu_red <himanshutdw98@gmail.com>
2023-01-13 14:42:04 +0530

    request mapping added to the api controller for blog

--- src/main/java/com/application/blog/api_controller/BlogApiController.java
+++ src/main/java/com/application/blog/api_controller/BlogApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ import com.application.blog.entity.Blog;
 import com.application.blog.service.BlogService;
 
 @RestController
-// @RequestMapping
+@RequestMapping("/application")
 public class BlogApiController {
     
     @Autowired
